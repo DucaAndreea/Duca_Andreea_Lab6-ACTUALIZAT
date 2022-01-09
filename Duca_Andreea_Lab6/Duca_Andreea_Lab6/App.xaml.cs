76c2fcaa4d88920ac7c496732fd6b53f45977902
ANDREEA-MARIA DUCA <andreea.maria.duca@stud.ubbcluj.ro>
2022-01-09 10:32:01 +0200

    Add project files.

---
+++ Duca_Andreea_Lab6/Duca_Andreea_Lab6/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Duca_Andreea_Lab6.Data;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Duca_Andreea_Lab6
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+
+    
+}
